661e8d37c0da27255792098be3abc0a5b304ff02
Helen Hood <helen.c.hood@gmail.com>
2014-08-25 15:08:59 -0400

    Initial commit

---
+++ server.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+#############################
+#         DATA
+#############################
+
+game_data = [
+  {
+    home_team: "Patriots",
+    away_team: "Broncos",
+    home_score: 7,
+    away_score: 3
+  },
+  {
+    home_team: "Broncos",
+    away_team: "Colts",
+    home_score: 3,
+    away_score: 0
+  },
+  {
+    home_team: "Patriots",
+    away_team: "Colts",
+    home_score: 11,
+    away_score: 7
+  },
+  {
+    home_team: "Steelers",
+    away_team: "Patriots",
+    home_score: 7,
+    away_score: 21
+  }
+]
+
+#############################
+#         METHODS
+#############################
+
+
+
+
+#############################
+#         ROUTES
+#############################
